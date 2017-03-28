7e912e08018eda33c004205c630c46d9b1c91b4d
Lu Yi <435977170@qq.com>
2017-03-28 22:45:57 +0800

    luyi at 6011 modify in 2017/03/28

--- LR.py
+++ LR.py
@@ -28,9 +28,33 @@ def gradientAscent(datas,labels):
         weights = weights + alpha*datas_mat.transpose()*errors          #梯度上升变形，朝着标准标签的方向缓慢移动
     return weights
 
+def plotBeatFit(weight,file_name):
+    import matplotlib.pyplot as plt
+    wet_array = weight.getA()
+    datas, labels = loadDatasFromFile(file_name)
+    datas = array(datas)
+    height = shape(datas)[0]
+    xcord1 = [];ycord1 = []; 
+    xcord2 = [];ycord2 = []; 
+    for i in range(height):
+        if int(labels[i]) == 1:
+            xcord1.append(datas[i][1]); ycord1.append(datas[i][2])
+        else:
+            xcord2.append(datas[i][1]); ycord2.append(datas[i][2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    x = arange(-3.0,3.0,0.1)
+    y = (-wet_array[0]-wet_array[1]*x)/wet_array[2]
+    ax.plot(x,y)
+    plt.xlabel('X1');plt.ylabel('X2'); 
+    plt.show()
 
 if __name__ == '__main__':
-    datas, labels = loadDatasFromFile('testSet.txt')
+    file_name = 'testSet.txt'
+    datas, labels = loadDatasFromFile(file_name)
     #print datas, labels
     weights = gradientAscent(datas,labels)
-    print weights
+    #print weights
+    plotBeatFit(weights,file_name)
